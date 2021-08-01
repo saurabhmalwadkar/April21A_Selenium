c68f1827449fa0ee622a481dc16c88360eeed6fa
RASHID <RASHID@DESKTOP-M1G4KTS>
2021-08-01 10:56:18 +0530

    [Saurabh]: demo2 class created

---
+++ src/sample1/demo2.java
@@ -0,0 +1,19 @@
+package sample1;
+
+public class demo2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hii");
+		
+		System.out.println("hii");
+		
+		System.out.println("hii");
+		
+		System.out.println("hii");
+		
+		System.out.println("hii");
+		
+	}
+	
+}
